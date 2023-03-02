2d3f496885e520fb275750f4023a537d20d9f81d
Noah-Silverstein <noahsilverstein@hotmail.com>
2023-03-02 14:21:15 +0100

    modified:   updatepositions.js 	modified:   index.html 	modified:   planetBuilder.js 	babylonplanet.js 	babylonplanetmaker.js 	test.js

--- updatepositions.js
+++ updatepositions.js
@@ -1,11 +1,57 @@
+const { Planet } = require('./planetBuilder.js');
+const { Moon } = require('./planetBuilder.js');
+const { Sun } = require('./planetBuilder.js');
+const { Orbit } = require('./planetBuilder.js');
+
+
+/*
+update position function (planetary system information, babylon meshes of all meshes in that planetary system)
+
+*/
+
+function createBabylonCelestialBody(scene, body, materialInfo){
+    var babylonCelestialBody = BABYLON.MeshBuilder.CreateSphere(body.name, { diameter: body.size }, scene);
+    var CelestialBodyMaterial = new BABYLON.StandardMaterial(body.name + "Material", scene);
+    babylonCelestialBody.material = CelestialBodyMaterial;
+    return babylonCelestialBody;
+}
+
 
 
 //renders planets and their moons 
 //the tick is the distance it should move 
-function planetarySystemMover(planet, tick) {
+//celestial body is the normal class, while the mesh is the corresponding babylon mesh
+//the satellitebodies are all the normal class bodies that orbit around the babylon mesh 
+//I'm not sure it's the best way to do this as you are kind of doubling up on the code but I want to keep the rendering/drawing
+//part of the code completely seperate from the info/logic part of the code that just means
+class BabylonCelestialBody {
+    constructor(celestialBody, celestialBodyMesh, satelliteBabylonCelestialBodies){
+        this.celestialBody = celestialBody;
+        this.celestialBodyMesh = celestialBodyMesh;
+        this.satelliteBabylonCelestialBodies = satelliteBabylonCelestialBodies;
+    }
+}
+
+
+
+/*
+bablyon solor system has that babylon sun/mesh which in turn has a planet/mesh which has moons/mesh 
+do both babylon sun and celestialsun need the sattelite info...probably right
+
+*/
+
+function updateSolarSystem(babylonCelestialBody, tick) {
+    let mesh = babylonCelestialBody.celestialBodyMesh;
+    let body = babylonCelestialBody.celestialBody;
+
+
+    mesh.position.x = planet.orbit.orbitLength * Math.cos(tick);
+    mesh.position.y = planet.orbit.orbitLength * Math.sin(tick);
+
     earth.position.x = 30 * Math.cos(tick);
     earth.position.z = 30 * Math.sin(tick);
     moon.position.x = earth.position.x + 10 * Math.cos(tick * 5);
     moon.position.z = earth.position.z + 10 * Math.sin(tick * 5);
     moon.rotation.y += 0.01;
 }
+
